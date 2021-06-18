ae4432ad2a9b85eb27b024a8e6657d70f042bb8f
NMShihab <nmshihabislam@gmail.com>
2021-06-18 23:24:25 +0600

    add product model

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user.apps.UserConfig',
+    'user',
     'product.apps.ProductConfig'
 ]
 
--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    name = models.CharField(max_length=256 , null= True, blank=True)
+    brand = models.CharField(max_length=256 , null= True, blank=True)
+    #image
+    category = models.CharField(max_length=256 , null= True, blank=True)
+    descripytion = models.TextField(null=True,blank=True)
+    rating = models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
+    numberOfReviews = models.IntegerField(null=True,blank=True,default=0)
+    price = models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
+    countInStock = models.IntegerField(null=True,blank=True,default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True,editable=False)
+
+    def __str__(self):
+        return self.name
