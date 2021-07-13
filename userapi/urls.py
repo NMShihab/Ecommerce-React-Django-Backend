1fd4a722d7b8c56d4c4612bad581410234839852
NMShihab <nmshihabislam@gmail.com>
2021-07-13 15:02:19 +0600

    add update view

--- userapi/urls.py
+++ userapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import  MyTokenObtainPairView,getUserProfile,getAllProfile,userRegister
+from .views import  MyTokenObtainPairView,getUserProfile,getAllProfile,userRegister,updateUserProfile
 
 urlpatterns = [ 
     path("signup/",userRegister,name="register"),
     path('login/', MyTokenObtainPairView.as_view(), name='login'),
     path("user/profile/",getUserProfile,name="userProfile"),
+    path("user/profile/update/",updateUserProfile,name="userProfileUpdate"),
     path("users",getAllProfile,name="users"),  
 ]
